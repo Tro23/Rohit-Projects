28fb06f45f596f6479abaf838b2d27d7af59da4f
Rohit Tamidapati <rohittamidapati11@gmail.com>
2020-03-18 12:04:59 -0600

    Add files via upload

---
+++ myfunc1.m
@@ -0,0 +1,59 @@
+function [sys, x0, str, ts] = myfunc1(t,x,u,flag)
+switch flag
+    case 0
+        [sys,x0,str,ts]=mdlIntializeSizes;
+    case 1
+        sys=mdlDerivatives(x,u,t);
+    case 3
+        sys=mdlOutputs(x,u,t);
+    case {2, 4, 9}
+        sys=[];
+    otherwise
+        error(['Unhandled flag = ', num2str(flag)]);
+end
+end
+
+function [sys, x0, str, ts] = mdlIntializeSizes
+sizes=simsizes;
+
+sizes.NumContStates=1;
+sizes.NumOutputs = 1;
+sizes.NumInputs = 0;
+
+sizes.NumDiscStates=0;
+sizes.DirFeedthrough=0;
+sizes.NumSampleTimes=1;
+sys=simsizes(sizes);
+str=[];
+ts=[0 0];
+x0= -0.0001;
+end
+
+    function sys=mdlDerivatives(x,u,t)
+        sys= 1/(2*x);
+        
+    end
+    function sys=mdlOutputs(x,u,t)
+        sys=x;
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ myfunc2.m
@@ -0,0 +1,44 @@
+function [sys, x0, str, ts] = myfunc2(t,x,u,flag)
+switch flag
+    case 0
+        [sys,x0,str,ts]=mdlIntializeSizes;
+    case 1
+        sys=mdlDerivatives(x,u,t);
+    case 3
+        sys=mdlOutputs(x,u,t);
+    case {2, 4, 9}
+        sys=[];
+    otherwise
+        error(['Unhandled flag = ', num2str(flag)]);
+end
+end
+
+function [sys, x0, str, ts] = mdlIntializeSizes
+sizes=simsizes;
+
+sizes.NumContStates=1;
+sizes.NumOutputs = 1;
+sizes.NumInputs = 0;
+
+sizes.NumDiscStates=0;
+sizes.DirFeedthrough=0;
+sizes.NumSampleTimes=1;
+sys=simsizes(sizes);
+str=[];
+ts=[0 0];
+x0= 0;
+end
+
+    function sys=mdlDerivatives(x,u,t)
+        sys= 1+(x^2);
+        
+    end
+    function sys=mdlOutputs(x,u,t)
+        sys=x;
+    end
+
+
+
+
+
+
